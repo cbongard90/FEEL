30362630a4f604df50a1735cc8d6e25715345b00
Christian Bongard <christian.bongard90@gmail.com>
2022-05-14 13:35:49 +0400

    added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  resources :boxes
+  resources :receivers
+  resources :locations
+  resources :packages
+  resources :timesheets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
