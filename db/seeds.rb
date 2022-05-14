ae9413d320b4379ef18d6717c287031ef42d7e5d
Ny Tsiky Razafindrabe <rova.nytsiky@gmail.com>
2022-05-15 02:07:32 +0400

    seeds part 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "open-uri"
+
 puts "Clearing database..."
 Package.destroy_all
 Location.destroy_all
+User.destroy_all
+
+# Seeds Users
+
+puts "Creating 1 admin"
+user1 = User.new(email: "test@test.com", password: "password", first_name:"Test", surname:"Test)
+file1 = URI.open('https://kitt.lewagon.com/placeholder/users/random')
+user1.photo.attach(io: file1, filename: "user1" , content_type: 'image/jpg')
+user1.save
+puts "Creating 1 admin"
+admin1 = User.new(email: "admin@test.com", password: "password", first_name:"Test", surname:"Test, admin: true)
+file1 = URI.open('https://kitt.lewagon.com/placeholder/users/random')
+admin1.photo.attach(io: file1, filename: "admin1" , content_type: 'image/jpg')
+admin1.save
+
+# Seeds Packages
 
 puts "Creating packages..."
 Package.create!(
